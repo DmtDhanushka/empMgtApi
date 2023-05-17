08453929aaeaf45cbc686ae28726e65be7b45f3f
Dimuthu.D <Dimuthu.D@creativesoftware.com>
2023-05-17 18:15:37 +0530

    Init

---
+++ src/main/java/com/emp/empmgtapi/EmpMgtApiApplication.java
@@ -0,0 +1,13 @@
+package com.emp.empmgtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpMgtApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emp/empmgtapi/EmpMgtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.empmgtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMgtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
